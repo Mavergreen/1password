bbf34ea6cb9b17f80e834883478e00534a16c846
Amitai Schleier <schmonz-web-git@schmonz.com>
2026-08-07 12:08:58 -0400

    Rename shared-cmake -> shipyard (CMake package, env var, refs)

--- build/release-notes-file.sh
+++ build/release-notes-file.sh
@@ -3,4 +3,4 @@ set -eu
 SELF="$(cd "$(dirname "$0")" && pwd)"
 MAVERICKS_ROOT="$(cd "$SELF/.." && pwd)"; export MAVERICKS_ROOT
 . "$SELF/msc.sh"
-exec sh "$MSC/release-notes-file.sh" "${1:?TAG required}" "${2:?FULL version required}" "Linux 1Password"
+exec sh "$SHIPYARD/release-notes-file.sh" "${1:?TAG required}" "${2:?FULL version required}" "Linux 1Password"
--- build/version.sh
+++ build/version.sh
@@ -3,4 +3,4 @@ set -eu
 SELF="$(cd "$(dirname "$0")" && pwd)"
 MAVERICKS_ROOT="$(cd "$SELF/.." && pwd)"; export MAVERICKS_ROOT
 . "$SELF/msc.sh"
-exec sh "$MSC/version.sh" "$@"
+exec sh "$SHIPYARD/version.sh" "$@"
